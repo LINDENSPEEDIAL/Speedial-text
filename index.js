965d87e91b71d9befb037ecde23117dfeca8b4f0
LINDENSPEEDIAL <INFO@SPEEDIAL.US>
2026-05-15 13:37:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post("/webhook", async (req, res) => {
         const reply = claudeResponse.data.content[0].text;
 
         await axios.post(
-          `https://graph.facebook.com/v18.0/${PHONE_NUMBER_ID}/messages`,
+          `https://graph.facebook.com/v20.0/${PHONE_NUMBER_ID}/messages`,
           {
             messaging_product: "whatsapp",
             to: userPhone,
